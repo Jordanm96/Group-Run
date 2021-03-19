24b11a84371f2cb18360306a019846235da8c6aa
Jordan Montero <jordanmontero@Jordans-MBP.attlocal.net>
2021-03-19 15:09:00 -0700

    Clean up code

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
-import { Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { baseURL, config } from "./services"; //This line is getting our api url
 import axios from "axios";
 import { useEffect, useState } from "react";
 import HomeRuns from "./components/HomeRuns";
 import RunData from "./components/RunData";
-import CreateNav from "./components/CreateNav";
 import NewRunForm from "./components/NewRunForm";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -30,9 +29,7 @@ function App() {
       <Route path="/">
         <Header />
       </Route>
-
       <Route exact path="/">
-        {/* <CreateNav /> */}
         <div className="home-container">
           <div className="mapped-container">
             {runs.map((run) => (
@@ -48,16 +45,13 @@ function App() {
           <Footer />
         </div>
       </Route>
-
       <Route path="/run/:id">
         <RunData runs={runs} />
       </Route>
-
       <Route exact path="/add">
         <h2 className="add-run-here">Add your run here!</h2>
         <NewRunForm runs={runs} setToggleFetch={setToggleFetch} />
       </Route>
-
       <Route path="/edit/:id">
         <h2 className="edit-run-here">Edit:</h2>
         <NewRunForm runs={runs} setToggleFetch={setToggleFetch} />
